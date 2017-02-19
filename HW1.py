4f933a856bedcd7f4a97b9c7a6efaff65478e1dc
Xiyao Ma <maxiy@ufl.edu>
2017-02-19 15:39:41 -0500

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,164 @@
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+
+
+# Convert class labels from scalars to one-hot vectors
+# 0 => [1 0 0 0 0 0 0 0 0 0]
+# 1 => [0 1 0 0 0 0 0 0 0 0]
+def dense_to_one_hot(labels_dense, num_classes):
+    num_labels = labels_dense.shape[0]
+    index_offset = np.arange(num_labels) * num_classes
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+
+# Read training data set (Train data from CSV file)
+data = pd.read_csv('train.csv')
+
+# Extracting images and labels from given data
+# For images
+images = data.iloc[:, 1:].values
+images = images.astype(np.float)
+
+# Normalize from [0:255] => [0.0:1.0]
+images = np.multiply(images, 1.0 / 255.0)
+image_size = images.shape[1]
+image_width = image_height = np.ceil(np.sqrt(image_size)).astype(np.uint8)
+
+# For labels
+labels_flat = data[[0]].values.ravel()
+labels_count = np.unique(labels_flat).shape[0]
+labels = dense_to_one_hot(labels_flat, labels_count)
+labels = labels.astype(np.uint8)
+
+train_images = images
+train_labels = labels
+
+epochs_completed = 0
+index_in_epoch = 0
+num_examples = train_images.shape[0]
+
+# visualisation variables
+train_accuracies = []
+validation_accuracies = []
+
+
+# Serve data by batches
+def next_batch(batch_size):
+    global train_images
+    global train_labels
+    global index_in_epoch
+    global epochs_completed
+
+    start = index_in_epoch
+    index_in_epoch += batch_size
+
+    # when all training data have been already used, it is reorder randomly
+    if index_in_epoch > num_examples:
+        # finished epoch
+        epochs_completed += 1
+        # shuffle the data
+        perm = np.arange(num_examples)
+        np.random.shuffle(perm)
+        train_images = train_images[perm]
+        train_labels = train_labels[perm]
+        # start next epoch
+        start = 0
+        index_in_epoch = batch_size
+        assert batch_size <= num_examples
+    end = index_in_epoch
+    return train_images[start:end], train_labels[start:end]
+
+
+'''initialization the input data'''
+sess = tf.InteractiveSession()
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+'''1st conv layer, conv + relu + max_pool'''
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+'''2nd conv layer, conv + relu + max_pool'''
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+'''Fully connected layer 1'''
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+'''Fully connected layer 3, softmax'''
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+# Cost function and training
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+sess.run(tf.global_variables_initializer())
+
+for i in range(25000):
+    batch = next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
+            x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g" % (i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+# read test data from CSV file
+test_images = pd.read_csv('test.csv').values
+test_images = test_images.astype(np.float)
+
+# convert from [0:255] => [0.0:1.0]
+test_images = np.multiply(test_images, 1.0 / 255.0)
+print('test_images({0[0]},{0[1]})'.format(test_images.shape))
+
+pred_y = tf.argmax(y_conv, 1)
+predictions = pred_y.eval(feed_dict={x: test_images, keep_prob: 1.0})
+
+
+# save results
+np.savetxt('submission.csv',
+           np.c_[range(1, len(test_images) + 1), predictions],
+           delimiter=',',
+           header='ImageId,Label',
+           comments='',
+           fmt='%d')
